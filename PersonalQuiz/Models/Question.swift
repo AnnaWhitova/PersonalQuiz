f4af0cd6be865b6b8c27540098a5368c8adb29fe
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-06-12 21:04:26 +0700

    work with project

---
+++ PersonalQuiz/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  PersonalQuiz
+//
+//  Created by Анна Белова on 02.06.2024.
+//
+
+import Foundation
---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Анна Белова on 31.05.2024.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
